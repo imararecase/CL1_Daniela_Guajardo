0a25d14e35867ff1df6214db5d4c55c0b42907db
DaniGuajardo <danign20@hotmail.com>
2023-09-28 15:27:54 -0500

    CL1_Daniela_Guajardo

---
+++ src/main/java/org/cibertec/edu/pe/Cl1DanielaGuajardoApplication.java
@@ -0,0 +1,13 @@
+package org.cibertec.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1DanielaGuajardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1DanielaGuajardoApplication.class, args);
+	}
+
+}
